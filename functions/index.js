f62869eaf061eecfc4290c0644be535cde6f5bca
GislaineJessica <GislainehJessica@gmail.com>
2019-08-29 08:57:32 -0300

    Registrando usuario no firebase (email, senha e handle) retornando um token de intificaçao

--- functions/index.js
+++ functions/index.js
@@ -1,27 +1,37 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const express = require('express')
-const app = express()
+const firebase = require('firebase')
+// const db = admin.firestore()
+const app = require('express')()
+const firebaseConfig = {
+    apiKey: "AIzaSyBp5xEiAOvtlhmy6LwMEt3UcSnysYk_trg",
+    authDomain: "meufirstproject.firebaseapp.com",
+    databaseURL: "https://meufirstproject.firebaseio.com",
+    projectId: "meufirstproject",
+    storageBucket: "meufirstproject.appspot.com",
+    messagingSenderId: "990480027010",
+    appId: "1:990480027010:web:7cfcaa5e0a8a9fbd"
+  };
 
+firebase.initializeApp(firebaseConfig)
 admin.initializeApp();
 
 app.get('/telas', (req, res) => {
-    admin.firestore()
-        .collection("telas")
-        .orderBy('createAt', 'desc')
-        .get()
-        .then( (data) => {
-            let telas = [];
-            data.forEach( (doc) => {
-                telas.push({
-                    telaId : doc.id,
-                    body : doc.data().body,
-                    userHandle : doc. data().userHandle,
-                    createAt : doc.data().createAt,
-                })
-            });
-        return res.json(telas);
-        })
+    admin.firestore().collection("telas")
+      .orderBy('createAt', 'desc')
+      .get()
+      .then( (data) => {
+         let telas = [];
+         data.forEach( (doc) => {
+            telas.push({
+            telaId : doc.id,
+            body : doc.data().body,
+            userHandle : doc. data().userHandle,
+            createAt : doc.data().createAt,
+            })
+        });
+    return res.json(telas);
+    })
         .catch( (erro => console.error(erro) ))
 })
 
@@ -31,16 +41,64 @@ app.post('/tela', (req, res) => {
         userHandle: req.body.userHandle,
         createAt: new Date().toISOString()
     };
-    admin.firestore()
-        .collection("telas")
-        .add(novaTela)
-        .then(doc => {
-            res.json({message:  `Documento ${doc.id} criado com sucesso`})
-        })
-        .catch( erro => {
-            res.status(500).json({erro : 'Algo deu errrado'})
-            console.error(erro) 
-        })
+    admin.firestore().collection("telas")
+      .add(novaTela)
+      .then(doc => {
+        res.json({message:  `Documento ${doc.id} criado com sucesso`})
+      })
+      .catch( erro => {
+        res.status(500).json({erro : 'Algo deu errrado'})
+        console.error(erro) 
+      })
+})
+
+let token;
+let userId;
+// Rota de registro
+app.post('/registrar', (req, res) => {
+    const novoUser = {
+        email: req.body.email,
+        senha: req.body.senha,
+        confirmSenha: req.body.confirmSenha,
+        handle: req.body.handle,
+    }
+
+    admin.firestore().doc(`/users/${novoUser.handle}`).get().then(doc => {
+        if (doc.exists)
+            return res.status(400).json({handle : "Esse usuário já existe"})
+        else{
+            return firebase.auth().createUserWithEmailAndPassword(novoUser.email, novoUser.senha)
+                .then( data => {
+                    userId = data.user.uid
+                    return data.user.getIdToken()
+                })
+                .then( (idToken) => {
+                    token = idToken
+                    const userCredencial = {
+                        handle:novoUser.handle,
+                        email:novoUser.email,
+                        createAt:new Date().toISOString(),
+                        userId
+                    }
+                    return admin.firestore().doc(`/users/${novoUser.handle}`).set(userCredencial)
+                })
+                .then(() =>{
+                    return res.status(201).json(token)
+                })
+                .catch(err => {
+                    console.error(err)
+                    if( err.code === "auth/email-already-in-use" ){
+                        return res.status(400).json({ email: "Email já está sendo usado"})
+                    }
+                    else{
+                        return res.status(500).json({error: err.code})
+                    }
+                })
+        }
+    })
+
+    // Proxima etapa validar entrada (Next)
+
 })
 
 exports.api = functions.https.onRequest(app);
@@ -88,5 +146,16 @@ exports.api = functions.https.onRequest(app);
                 console.error(erro) 
             })
     })
+    ///////////////////////
+        firebase
+        .auth()
+        .createUserWithEmailAndPassword(novoUser.email, novoUser.senha)
+        .then(data => {
+            return res.status(201).json({message : `Usuário ${data.user.uid} criado com sucesso!`})
+        })
+        .catch(erro => {
+            console.error(erro)
+            return res.status(500).json({ wrong: erro.code})
+        })
  */
 }
