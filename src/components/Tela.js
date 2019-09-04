ca684e9bc312461efcb44ae506ee229fd2ddc62a
GislaineJessica <GislainehJessica@gmail.com>
2019-09-04 16:16:22 -0300

    Correçao da imagem na tela

--- src/components/Tela.js
+++ src/components/Tela.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+
 import withStyles from '@material-ui/core/styles/withStyles'
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-import Link from 'react-router-dom/Link'
+
+import Link from 'react-router-dom/Link';
 
 
 const style = {
@@ -23,15 +25,16 @@ const style = {
 
 class Tela extends Component {
   render() {
-    const {classes, tela: {body, createAt, userHandle, imageUrl}} = this.props
+    const  {classes, tela: {body, createAt, userHandle, userImage}} = this.props
+    console.log(this.props)
     
     return ( 
-      <Card className = {classes.Card}>
-        <CardMedia className = {classes.image} image = {imageUrl} title = 'Imagem Perfil' />
+      <Card className = {classes.card}>
+        <CardMedia className = {classes.image} image = {userImage} title = "Imagem Perfil" />
         <CardContent className = {classes.content}>
-          <Typography variant= 'h5'  color = 'primary' component = {Link} to = {`/user/${userHandle}`}> {userHandle} </Typography>
-          <Typography variant= 'body2' color ='textSecundary'> {createAt} </Typography>
-          <Typography variant= 'body1'> {body} </Typography>
+          <Typography variant= "h5"  color = "primary" component = {Link} to = {`/user/${userHandle}`}> {userHandle} </Typography>
+          <Typography variant= "body2" color ="textSecondary"> {createAt} </Typography>
+          <Typography variant= "body1"> {body} </Typography>
         </CardContent>
       </Card>
     )
